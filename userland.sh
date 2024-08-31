f3642821df6c1b81ae7b6397aab59b5f2c680b45
Shen367 <sastrakasiashen@gmail.com>
2024-08-31 15:54:22 +0700

    Add files via upload

---
+++ userland.sh
@@ -0,0 +1,174 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay sastra
+neofetch --ascii --ascii_distro alphine
+echo $cyan note kalo mau jalanin yang di menu
+echo $purple install debian dulu kalo gk,gk akan bisa
+echo $white jika masih eror proot-distro reset ^nama sistem^
+echo $green "selamat datang"
+sleep 2
+echo "menu pilihan anda"
+echo
+echo "1:login debian"
+echo
+echo "2:login fedora"
+echo
+echo "3:logim pardus"
+echo
+echo "4:login ubuntu"
+echo
+echo "5:wa pembuat"
+echo
+echo "6:login void"
+echo
+echo "7:cara jadi pro hack"
+echo
+echo "8:ramalan kiamat"
+echo
+echo "9:login manjaro"
+echo
+echo "10:login opensuse"
+echo
+echo "11:login deepin"
+echo
+echo "12:login artix"
+echo
+echo "13:login alpine"
+echo
+echo "14:login archlinux"
+echo
+read -p "PilihMana :" bro
+if [ $bro = 1 ] || $bro = 1 ]
+then
+clear
+toilet -f big -F gay By Neutra
+echo $red menginstall debian
+pkg install proot
+pkg install proot-distro
+proot-distro install debian
+proot-distro login debian
+fi
+
+if [ $bro = 2 ] || $bro = 2 ]
+then
+clear
+toilet -f big -F gay SASTRA
+pkg install proot
+pkg install proot-distro
+proot-distro install fedora
+proot-distro login fedora
+fi
+
+if [ $bro = 3 ] || $bro = 3 ]
+then
+clear
+toilet -f big -F gay Sastra
+pkg install proot
+pkg install proot-distro 
+proot-distro install pardus
+prooo-distro login pardus
+fi
+
+if [ $bro = 4 ] || $bro = 4 ]
+then
+clear
+pkg install proot
+pkg install proot-distro 
+proot-distro install ubuntu
+proot-distro login ubuntu
+fi
+
+if [ $bro = 5 ] || $bro = 5 ]
+then
+clear
+echo number wa gweh +6281277769327
+fi
+
+if [ $bro = 6 ] || $bro = 6 ]
+then
+clear
+pkg install proot
+pkg install proot-distro
+proot-distro install void
+proot-distro login void
+fi
+
+if [ $bro = 7 ] || $bro = 7 ]
+then
+clear
+toilet -f big -F gay motivasi
+echo $purple ingin jadi hacker pro kek gw
+echo $purple ada syarat
+echo 1:tidak berpacaran
+echo 2:tidak menindas yang kecil
+echo 3:fast respon
+echo 4:mental kuat
+fi
+
+if [ $bro = 8 ] || $bro = 8 ]
+then
+clear
+echo $blue sungguh hanya tuhan yang tahu kiamat
+fi
+
+if [ $bro = 9 ] || $bro = 9 ]
+then
+clear
+pkg install proot
+pkg install proot-distro
+proot-distro install manjaro
+proot-distro login manjaro
+fi
+
+if [ $bro = 10 ] || $bro = 10 ]
+then
+clear
+pkg install proot
+pkg install proot-distro
+proot-distro install opensuse
+proot-distro login opensuse
+fi
+
+if [ $bro = 11 ] || $bro = 11 ]
+then
+clear
+pkg install proot
+pkg install proot-distro
+proot-distro install deepin
+proot-distro login deepin
+fi
+if [ $bro = 12 ] || $bro = 12 ]
+then
+clear
+pkg install proot
+pkg install proot-distro
+proot-distro install artix
+proot-distro login artix
+fi
+
+if [ $bro = 13 ] || $bro = 13 ]
+then
+clear
+pkg install proot
+pkg install proot-distro
+proot-distro install alpine
+proot-distro logijn alpine
+fi
+
+if [ $bro = 14 ] || $bro = 14 ]
+then
+clear
+pkg install proot
+pkg install proot-distro
+proot-distro install archlinux
+proot-distro login archlinux
+fi
+
+
